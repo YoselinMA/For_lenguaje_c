fa2e58882c9b24304260ca79ba454c456dffb346
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-10-04 23:51:09 -0600

    Create longitud.c

---
+++ longitud.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int longitudSerie = 50;
+    int i;
+    for(i = 1;  i<=(longitudSerie/2); i++){
+        printf("%d\n ", 2*i);
+        printf("%d\n ", 3*i);
+    }
+
+}
