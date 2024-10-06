1fc545905f2c1210ed37dd6616fad48773dbd9d9
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-10-05 23:32:11 -0600

    Create acomodo.c

---
+++ acomodo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int i, j;
+
+    for (i=0; i<5; i ++){
+        printf("para i: %d \t", i);
+        for (j=0; j<3; j++){
+            printf("j%d,", j);
+
+        }
+        printf("\n\n");
+    }
+}
