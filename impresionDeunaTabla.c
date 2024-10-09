81e3dfba1eb1c467ddba9b84ff3d75a965d8205d
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-10-08 22:20:34 -0600

    Create impresionDeunaTabla.c

---
+++ impresionDeunaTabla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    for(a = 1; a >=1; a--){
+        printf("A   A+2   A+4   A+6\n");
+        printf("3    5     7     9\n");
+        printf("6    8     10    12\n");
+        printf("9    11    13    15\n");
+        printf("12   14    16    18\n");
+    }
+}
